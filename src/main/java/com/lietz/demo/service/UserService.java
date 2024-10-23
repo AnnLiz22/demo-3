08441934a16323a279da864a0131b1ff9a4948cf
AnnLiz22 <bielakam@gmail.com>
2024-10-23 22:35:44 +0200

    implementing getUserById and editUser; refactor with required arguments constructor

--- src/main/java/com/lietz/demo/service/UserService.java
+++ src/main/java/com/lietz/demo/service/UserService.java
@@ -4,29 +4,29 @@ import com.lietz.demo.model.User;
 import com.lietz.demo.repo.UserRepo;
 import java.util.List;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Getter
+@RequiredArgsConstructor
 public class UserService {
-
-  private UserRepo repo;
-
-  @Autowired
-  public void setRepo(UserRepo repo) {
-    this.repo = repo;
-  }
+  private final UserRepo repo;
 
   public User addUser(User user) {
     repo.save(user);
     return user;
   }
 
-  public User editUser(User user){
+  public User getUserById(Long id){
+    return repo.getById(id);
+  }
+
+  public int editUser(User user){
     System.out.println(user + " updated");
-    return user;
+    return repo.updateUser(user);
   }
 
   public List<User> findAll(){
