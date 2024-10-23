2b6f90ff21ab3049d1cdb43feb99f1499cf1d56b
AnnLiz22 <bielakam@gmail.com>
2024-10-23 22:33:43 +0200

    added getById and update user method; refactor

--- src/main/java/com/lietz/demo/repo/UserRepo.java
+++ src/main/java/com/lietz/demo/repo/UserRepo.java
@@ -17,23 +17,36 @@ public class UserRepo {
   private final JdbcTemplate template;
 
   public void save(User user) {
-  String query = "INSERT INTO users (id, name, role) VALUES (?,?,?)";
-  int rows = template.update(query, user.getId(), user.getName(), user.getRole());
+    String query = "INSERT INTO users (id, name, role) VALUES (?,?,?)";
+    int rows = template.update(query, user.getId(), user.getName(), user.getRole());
     System.out.println(rows + " effectuated");
   }
 
-  public User update(User user) {
-    //
-    return user;
+  public User getById(Long id) {
+    String query = "SELECT * from users where id = ?";
+
+    RowMapper<User> mapper = (rs, rowNum) -> {
+      User user = new User();
+      user.setId(rs.getLong("id"));
+      user.setName(rs.getString("name"));
+      user.setRole(rs.getString("role"));
+      return user;
+    };
+    return template.queryForObject(query, new Object[] {id}, mapper);
+  }
+
+  public int updateUser(User user) {
+    String query = "UPDATE users SET name = ?, role = ? WHERE id = ?";
+    return template.update(query, user.getName(), user.getRole(), user.getId());
   }
 
   public List<User> findAll() {
-    String query = "Select * from users";
+    String query = "SELECT * from users";
 
     RowMapper<User> mapper = (rs, rowNum) -> {
 
       User user = new User();
-      user.setId(rs.getInt("id"));
+      user.setId(rs.getLong("id"));
       user.setName(rs.getString("name"));
       user.setRole(rs.getString("role"));
 
@@ -42,8 +55,13 @@ public class UserRepo {
     return template.query(query, mapper);
   }
 
-  public void removeByName(String userName){
-    String query = "delete from users where name like ?";
+  public void removeByName(String userName) {
+    String query = "DELETE from users where name = ?";
+
+  }
+
+  public void removeById(Long id) {
+    String query = "DELETE from users where id = ?";
 
   }
 }
