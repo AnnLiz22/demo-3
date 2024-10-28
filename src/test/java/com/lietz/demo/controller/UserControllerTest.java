438d42947e039942f55c8a66747bfb2b8678bbb9
AnnLiz22 <bielakam@gmail.com>
2024-10-28 19:55:33 +0100

    updated after method refactor

--- src/test/java/com/lietz/demo/controller/UserControllerTest.java
+++ src/test/java/com/lietz/demo/controller/UserControllerTest.java
@@ -37,7 +37,7 @@ class UserControllerTest {
     User updatedUser = new User(1L, "Ania", "my new role");
 
     when(userService.getUserById(1L)).thenReturn(Optional.of(user));
-    when(userService.updateUser(1L)).thenReturn(Optional.of(updatedUser));
+    when(userService.updateUser(1L, user.getName(), user.getRole())).thenReturn(Optional.of(updatedUser));
 
     mockMvc.perform(put("/api/users/edit-user/1")
             .contentType(MediaType.APPLICATION_JSON)
