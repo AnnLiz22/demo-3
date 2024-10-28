68724173e80e1cd95590a75b7a3eb220bfda90ec
AnnLiz22 <bielakam@gmail.com>
2024-10-28 21:17:31 +0100

    added id annotation, task list, constructor

--- src/main/java/com/lietz/demo/model/User.java
+++ src/main/java/com/lietz/demo/model/User.java
@@ -1,12 +1,17 @@
 package com.lietz.demo.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
+
 @Component
 @Scope(value = "prototype")
 @Getter
@@ -15,9 +20,16 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String role;
+  private List<Task> tasks;
 
+  public User(Long id, String name, String role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
 }
