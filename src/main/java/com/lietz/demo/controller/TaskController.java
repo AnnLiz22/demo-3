261ec6c7e1cf93d390bbc80da6f38a9f23c68358
AnnLiz22 <bielakam@gmail.com>
2024-10-29 22:50:14 +0100

    added endpoints for all methods

--- src/main/java/com/lietz/demo/controller/TaskController.java
+++ src/main/java/com/lietz/demo/controller/TaskController.java
@@ -1,13 +1,16 @@
 package com.lietz.demo.controller;
 
 import com.lietz.demo.model.Task;
-import com.lietz.demo.model.User;
 import com.lietz.demo.service.TaskService;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,4 +28,43 @@ public class TaskController {
       return ResponseEntity.notFound().build();
     }
   }
+
+  @RequestMapping(value = "/add", method = RequestMethod.POST)
+  public Task addTask(@RequestBody Task task){
+    Task savedTask = taskService.addTask(task);
+    return savedTask;
+  }
+
+  @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+    Optional<Task> updated = taskService.updateTask(id, updatedTask);
+
+    if (updated.isPresent()) {
+      return ResponseEntity.ok(updated.get());
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+  @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity <Task> deleteTask(@PathVariable Long id){
+    Optional<Task> optionalTask = taskService.getById(id);
+    if(optionalTask.isPresent()){
+      taskService.deleteTask(id);
+      return ResponseEntity.noContent().build();
+    }else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+  @RequestMapping(value = "", method = RequestMethod.GET)
+  public ResponseEntity<List<Task>> findTasks(){
+    List<Task> tasks = taskService.findAllTasks();
+    if(tasks.isEmpty()){
+      return ResponseEntity.noContent().build();
+    }else {
+      return ResponseEntity.ok(tasks);
+    }
+  }
+
 }
