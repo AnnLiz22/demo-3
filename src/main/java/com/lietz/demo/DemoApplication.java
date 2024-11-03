e5d6f9d7f3f6e7d2cb2b9ddb6b826e077c8c1220
AnnLiz22 <bielakam@gmail.com>
2024-11-03 21:36:53 +0100

    testing lines removed from app class

--- src/main/java/com/lietz/demo/DemoApplication.java
+++ src/main/java/com/lietz/demo/DemoApplication.java
@@ -1,32 +1,18 @@
 package com.lietz.demo;
 
 import com.lietz.demo.model.User;
-import com.lietz.demo.repo.UserRepo;
 import com.lietz.demo.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-
 		System.out.println("hello there");
-
 		User user = context.getBean(User.class);
-		user.setId(7L);
-		user.setName("Hanna");
-		user.setRole("mama");
-
 		UserService service = context.getBean(UserService.class);
-		service.addUser(user);
-
-
-		System.out.println(service.findAll());
-		service.deleteUserByName(user.getName());
 	}
-
 }
